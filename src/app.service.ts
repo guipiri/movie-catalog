0f68b55b88eeb6207ed34bf1674c77c912f8a4e9
xuxa <gui.soliveiras@gmail.com>
2024-06-19 16:28:38 -0300

    ci/cd: test workflow

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Here you will be able to manage your movies!';
+    return 'Here you will be able to manage your movies!!';
   }
 }
