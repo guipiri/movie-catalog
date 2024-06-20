ac64e9615b852037c23f0b00d49eeebdf7dbf4af
xuxa <gui.soliveiras@gmail.com>
2024-06-20 18:18:29 -0300

    build: install packages and initialize the import on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -10,6 +12,16 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+
+    CacheModule.registerAsync({
+      useFactory: (congigService: ConfigService) => ({
+        store: redisStore,
+        host: congigService.get<string>('REDIS_HOST'),
+        port: +congigService.get<number>('REDIS_PORT'),
+        ttl: 0,
+        max: 100,
+      }),
+    }),
     DbModule,
     MovieModule,
     AuthModule,
