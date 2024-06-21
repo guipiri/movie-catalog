f519564655bb4431745e5a2e273696841f07e40f
xuxa <gui.soliveiras@gmail.com>
2024-06-21 11:17:55 -0300

    ci/cd: test new ec2 vm with more ssd memory again

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.APP_PORT, () => {
-    console.log(`Server running on port ${process.env.APP_PORT}!!`);
+    console.log(`Server running on port ${process.env.APP_PORT}!`);
   });
 }
 bootstrap();
